90ac46a673c789048fb50bc69c1bb3239e4ab12a
Johnpaul <indisputable.jay@gmail.com>
2024-03-21 23:11:49 +0100

    added tons of new features

--- assignments/01-assignment/fizz-buzz.js
+++
@@ -1,9 +0,0 @@
-const fizzBuzz = (input) => {
-  if (typeof input !== 'number') return 'Not a number';
-
-  if (input % 3 === 0 && input % 5 === 0) return 'FizzBuzz';
-  if (input % 3 === 0) return 'Fuzz';
-  if (input % 5 === 0) return 'Buzz';
-};
-
-export default fizzBuzz;
--- assignments/01-assignment/index.js
+++ assignments/01-assignment/index.js
@@ -1,7 +1,28 @@
-import fizzBuzz from './fizz-buzz';
+const fizzBuzz = (input) => {
+  //
+  if (input % 3 === 0 && input % 5 === 0) return 'FizzBuzz';
+  if (input % 3 === 0) return 'Fuzz';
+  if (input % 5 === 0) return 'Buzz';
+
+  return `${input} is not divisible by either 3 or 5`;
+};
+
+const solution = () => {
+  const value = parseInt(number.value, 10);
+  if (!value) return;
+  console.log(value);
+  alert(fizzBuzz(value));
+  number.value = null;
+};
 
 const number = document.getElementById('number');
+const button = document.getElementById('button');
 
-console.log(number);
+button.addEventListener('click', () => {
+  solution();
+});
 
-if (number.value) alert(fizzBuzz(number.value));
+document.addEventListener('keydown', (key) => {
+  if (key.key !== 'Enter') return;
+  solution();
+});
